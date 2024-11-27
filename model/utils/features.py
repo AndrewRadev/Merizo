4e9ec9ed68cc224e3a6bfae64470c86951389607
Andrew Radev <andrey.radev@gmail.com>
2024-11-27 22:04:16 +0100

    Add histidine variants to residue dict

--- model/utils/features.py
+++ model/utils/features.py
@@ -22,7 +22,7 @@ resndict = {'ALA': 'A', 'ARG': 'R', 'ASN': 'N', 'ASP': 'D', 'CYS': 'C',
             'GLN': 'Q', 'GLU': 'E', 'GLY': 'G', 'HIS': 'H', 'ILE': 'I',
             'LEU': 'L', 'LYS': 'K', 'MET': 'M', 'PHE': 'F', 'PRO': 'P',
             'SER': 'S', 'THR': 'T', 'TRP': 'W', 'TYR': 'Y', 'VAL': 'V',
-            'PAD': 'X'}
+            'PAD': 'X', 'HID': 'H', 'HIP': 'H', 'HIE': 'H'}
 
 aa_trans = str.maketrans('ARNDCQEGHILKMFPSTWYVBJOUXZ-.',
                          'ABCDEFGHIJKLMNOPQRSTUUUUUUVV')
